56b1fdc1cc5547d504faec756a1390f1563459d3
hratika1264 <vidhi63987@gmail.com>
2024-05-25 08:59:55 +0530

    Add files via upload

---
+++ question 2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+//2.Print "Hello" in the first line and "KGI !!" in the second line.#include <stdio.h>
+//hratika verma
+    printf("Hello\nKGI !!\n");
+    return 0;
+}
